4b4138d759d19501e80d20e5bb6c2e20de8587ea
shelei <shelei@huya.com>
2019-11-05 16:55:08 +0800

    实现基本框架和一个扫描的特效

---
+++ gl_sweep_effect_widget.cpp
@@ -0,0 +1,6 @@
+#include "gl_sweep_effect_widget.h"
+
+gl_sweep_effect_widget::gl_sweep_effect_widget()
+{
+
+}
---
+++ gl_sweep_effect_widget.h
@@ -0,0 +1,11 @@
+#ifndef GL_SWEEP_EFFECT_WIDGET_H
+#define GL_SWEEP_EFFECT_WIDGET_H
+
+
+class gl_sweep_effect_widget : public GLQuadWidget
+{
+public:
+    gl_sweep_effect_widget();
+};
+
+#endif // GL_SWEEP_EFFECT_WIDGET_H
